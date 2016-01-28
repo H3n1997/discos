44d015c408fefaa4f3f8196421af7d9d447a7546
H3n1997 <henry_bm76@hotmail.com>
2016-01-28 14:03:37 -0600

    inicio de repositorio

---
+++ discos.js
@@ -0,0 +1,16 @@
+$(document).on('ready', inicia);
+
+function inicia()
+{
+	$('button').on('click',compra);
+}
+
+function compra()
+{
+	var nodo=$(this).parent().children().first().next().text();
+
+	$('aside').append('<li>'+nodo+'</li>');
+	var total=parseInt($(this).parent().children().first().next().next().next().text());
+	total1=$('aside').append(total);
+	alert(total);
+}
