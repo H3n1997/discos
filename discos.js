bfe9f02e03b07138731ea6ffeed2a12b28933dcf
H3n1997 <henry_bm76@hotmail.com>
2016-01-28 14:18:37 -0600

    hasta aqui va chevere

--- discos.js
+++ discos.js
@@ -10,7 +10,9 @@ function compra()
 	var nodo=$(this).parent().children().first().next().text();
 
 	$('aside').append('<li>'+nodo+'</li>');
-	var total=parseInt($(this).parent().children().first().next().next().next().text());
-	total1=$('aside').append(total);
-	alert(total);
+	// var total=parseInt($(this).parent().children().first().next().next().next().text());
+	// total1=$('aside').append(total);
+	// alert(total);
+	var precio=$(this).parent().children().first().next().next().next().text());
+	precio=parseInt(precio);
 }
